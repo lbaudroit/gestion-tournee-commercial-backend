1e1ce91d132c2e5360b2348d16cd26473aa79479
enzo2900 <enzo.cluzel@iut-rodez.fr>
2024-11-27 16:21:22 +0100

    feat : Ajout de Spring à partir de Spring initializer

---
+++ src/main/java/friutrodez/backendtourneecommercial/BackendtourneecommercialApplication.java
@@ -0,0 +1,13 @@
+package friutrodez.backendtourneecommercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtourneecommercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtourneecommercialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/friutrodez/backendtourneecommercial/BackendtourneecommercialApplicationTests.java
@@ -0,0 +1,13 @@
+package friutrodez.backendtourneecommercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtourneecommercialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
