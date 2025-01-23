80df23643a53c70d2f12d265e2be48d4d2ef20b7
Leïla BAUDROIT <109075845+lbaudroit@users.noreply.github.com>
2025-01-23 10:00:34 +0100

    refactor: code inutile dans ApplicationConfiguration.java

--- src/main/java/friutrodez/backendtourneecommercial/configuration/ApplicationConfiguration.java
+++ src/main/java/friutrodez/backendtourneecommercial/configuration/ApplicationConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -45,10 +44,5 @@ public class ApplicationConfiguration {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
