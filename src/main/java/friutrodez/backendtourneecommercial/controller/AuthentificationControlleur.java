3b6341f0f5ed12794d4bf5260e08e66aa9ea7891
Chaipo <ahmed.bribach@iut-rodez.fr>
2025-01-29 10:47:02 +0100

    fix : ajout commentaire javadoc

--- src/main/java/friutrodez/backendtourneecommercial/controller/AuthentificationControlleur.java
+++ src/main/java/friutrodez/backendtourneecommercial/controller/AuthentificationControlleur.java
@@ -5,37 +5,48 @@ import friutrodez.backendtourneecommercial.service.AuthentificationService;
 import friutrodez.backendtourneecommercial.dto.DonneesAuthentification;
 import friutrodez.backendtourneecommercial.service.JwtService;
 import friutrodez.backendtourneecommercial.model.Utilisateur;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-
-import java.security.Principal;
 
 
 /**
  * Controlleur pour gérer les requêtes gérant l'authentification et la gestion de compte
+ *
+ *
+ * @author Benjamin NICOL
+ * @author Enzo CLUZEL
+ * @author Leïla BAUDROIT
+ * @author Ahmed BRIBACH
  */
 @RequestMapping(path = "/auth")
 @RestController
 @Validated
-public class AuthentificationController {
+public class AuthentificationControlleur {
 
     private final JwtService jwtService;
 
     private final AuthentificationService authentificationService;
 
+    /**
+     * Constructeur de la classe AuthentificationControlleur
+     *
+     * @param jwtService Service pour la gestion des tokens JWT
+     * @param authentificationService Service d'authentification des utilisateurs
+     */
     @Autowired
-    public AuthentificationController(JwtService jwtService, AuthentificationService authentificationService) {
+    public AuthentificationControlleur(JwtService jwtService, AuthentificationService authentificationService) {
         this.jwtService = jwtService;
         this.authentificationService = authentificationService;
     }
 
+    /**
+     * Authentifie un utilisateur en fonction de ses données d'authentification.
+     * @param donneesAuthentification
+     * @return ResponseEntity contenant un token JWT si l'authentification réussit, sinon une réponse d'erreur
+     */
     @PostMapping(path = "/authentifier")
     public ResponseEntity<JwtToken> authentifier(@RequestBody DonneesAuthentification donneesAuthentification) {
         Utilisateur utilisateur = authentificationService.authentifier(donneesAuthentification);
@@ -46,7 +57,12 @@ public class AuthentificationController {
 
     }
 
-
+    /**
+     * Crée un compte utilisateur.
+     *
+     * @param utilisateur Objet contenant les informations du nouvel utilisateur
+     * @return ResponseEntity contenant l'utilisateur créé ou un message d'erreur si la création échoue
+     */
     @PostMapping(path = "/creer")
     public ResponseEntity CreerUnCompte(@RequestBody Utilisateur utilisateur) {
         Utilisateur utilisateurCreer =  authentificationService.creerUnCompte(utilisateur);
--- src/main/java/friutrodez/backendtourneecommercial/controller/UtilisateurControlleur.java
+++ src/main/java/friutrodez/backendtourneecommercial/controller/UtilisateurControlleur.java
@@ -8,6 +8,14 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Rest Controlleur de la ressource Utilisateur
+ *
+ * @author Benjamin NICOL
+ * @author Enzo CLUZEL
+ * @author Leïla BAUDROIT
+ * @author Ahmed BRIBACH
+ */
 @RequestMapping(path = "/utilisateur/")
 @RestController
 public class UtilisateurControlleur {
@@ -15,6 +23,13 @@ public class UtilisateurControlleur {
     @Autowired
     UtilisateurRepository utilisateurRepository;
 
+    /**
+     * Modifie les informations de l'utilisateur authentifié.
+     *
+     * @param utilisateurModif Objet contenant les nouvelles informations de l'utilisateur.
+     * @return ResponseEntity contenant l'utilisateur modifié ou un message d'erreur en cas d'échec.
+     * @throws IllegalArgumentException si l'objet utilisateurModif est null.
+     */
     @PostMapping(path = "modifier")
     public ResponseEntity<Utilisateur> modifierUtilisateur(@RequestBody Utilisateur utilisateurModif) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -23,6 +38,11 @@ public class UtilisateurControlleur {
         return ResponseEntity.ok().body(utilisateurRepository.save(utilisateurModif));
     }
 
+    /**
+     * Supprime le compte de l'utilisateur authentifié.
+     *
+     * @return ResponseEntity avec un message de confirmation ou une erreur si l'opération échoue.
+     */
     @DeleteMapping(path = "supprimer")
     public  ResponseEntity<String> supprimerUtilisateur() {
         Utilisateur utilisateur = (Utilisateur) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
